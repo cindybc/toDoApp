976f0eb13185c3b2f12119a17ff0d432cfe7b19f
cindy <cindialy@gmail.com>
2017-06-29 11:21:45 -0400

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
+
 $(document).ready(function(){
-	$("button").click(function(e){
+	var contador = 0;
+	$("#add").click(function(e){
 		e.preventDefault();
+		
 		var tarea= $("#ingresaTarea").val();
 		if(tarea == ""){
 			alert("Debes ingresar una tarea");
 			 return false;
 		}
-
-		var contenido ='<div class="nuevo"><input type="checkbox" class="filled-in" id="box"/><label for="box"></label>' + tarea + '<button class="btn right botoncito">Remove</button></div>';
+        
+		var contenido ='<div class="nuevo"><input type="checkbox" class="filled-in" id="box'+ contador+'"/><label for="box'+ contador+'"></label>' + tarea + '<button class="btn right botoncito">Remove</button></div>';
 		$('#agregarTarea').append(contenido);
 		$("#ingresaTarea").val("");
-
+		contador++;
+        
 	
 		$(document).on('click', '.botoncito', function(){
 			$(this).parent().remove();
